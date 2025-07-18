5523d2a590651fdff8d4c491c7d21ac43479ae9a
arunci <96084440+arunci@users.noreply.github.com>
2025-07-19 00:57:59 +0200

    game in fullscreen mode

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -16,16 +17,22 @@ class Ship:
         # Start each new ship at the botton center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a flat for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flag; start with a ship that is not moving
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship position based in the movement flags."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""

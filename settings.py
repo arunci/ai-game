9180fdd7c81f629f55d9dc5bc26f5894ada82dd6
arunci <96084440+arunci@users.noreply.github.com>
2025-07-27 15:59:10 +0200

    show high score

--- settings.py
+++ settings.py
@@ -19,10 +19,12 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quicky the alien point values increase.
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,7 +39,10 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
